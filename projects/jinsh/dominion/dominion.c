65f8f6ccd46ef18994ae47a137cf81911eee2c23
carolflyjs <jinsh@oregonstate.edu>
2018-07-07 20:59:20 -0700

    Add externalized Adventurer, no testing done

--- projects/jinsh/dominion/dominion.c
+++ projects/jinsh/dominion/dominion.c
@@ -643,6 +643,34 @@ int getCost(int cardNumber)
   return -1;
 }
 
+
+int adventurerEffect(struct gameState *state, int handPos)
+{
+  int drawntreasure = 0;
+  int currentPlayer = whoseTurn(state);
+  int cardDrawn;
+  int z = 0;
+  while(drawntreasure<2){
+    if (state->deckCount[currentPlayer] < 1){
+      shuffle(currentPlayer, state);
+    }
+    drawCard(currentPlayer, state);
+    cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];
+    if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold)
+      drawntreasure++;
+    else {
+      temphand[z] = cardDrawn;
+      state->handCount[currentPlayer]--;
+      z++;
+    }
+    while (z - 1 >= 0) {
+      state->discard[currentPlayer][state->discardCount[currentPlayer]++] = template[z-1];
+      z = z - 1;
+    }
+  }
+  return 0;
+}
+
 int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState *state, int handPos, int *bonus)
 {
   int i;
@@ -667,25 +695,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
   switch( card ) 
     {
     case adventurer:
-      while(drawntreasure<2){
-	if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
-	  shuffle(currentPlayer, state);
-	}
-	drawCard(currentPlayer, state);
-	cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];//top card of hand is most recently drawn card.
-	if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold)
-	  drawntreasure++;
-	else{
-	  temphand[z]=cardDrawn;
-	  state->handCount[currentPlayer]--; //this should just remove the top card (the most recently drawn one).
-	  z++;
-	}
-      }
-      while(z-1>=0){
-	state->discard[currentPlayer][state->discardCount[currentPlayer]++]=temphand[z-1]; // discard all cards in play that have been drawn
-	z=z-1;
-      }
-      return 0;
+      return adventureEffect(state, handPos);
 			
     case council_room:
       //+4 Cards
