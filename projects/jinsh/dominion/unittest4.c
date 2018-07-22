1fbf561c737739301a3053286740b946a6c2db7d
carolflyjs <jinsh@oregonstate.edu>
2018-07-22 16:38:10 -0700

    updated unittest4

--- projects/jinsh/dominion/unittest4.c
+++ projects/jinsh/dominion/unittest4.c
@@ -8,22 +8,36 @@ void testIsGameOver(){
 	
 
 	struct gameState *state = (struct gameState *) malloc (sizeof(struct gameState));
-
+	int kingdomCards[10] = {adventurer, gardens, embargo, village, minion, mine, cutpurse, sea_hag, tribute, smithy};
+ 
+  initializeGame(3, kingdomCards, 6, state);
+  if (!isGameOver(state))
+    printf("testIsGameOver(): PASS when game is not over\n");
+	else
+		printf("testIsGameOver(): FAIL when game is not over\n");
+  
 	state->supplyCount[province] = 0;
 	if (isGameOver(state))
 		printf("testIsGameOver(): PASS when no province is left\n");
 	else
 		printf("testIsGameOver(): FAIL when no province is left\n");
-	
+
 	state->supplyCount[province] = 3;
 	state->supplyCount[adventurer] = 0;
 	state->supplyCount[gold] = 0;
-	state->supplyCount[gardens] = 0;
-	if (isGameOver(state))
-		printf("testIsGameOver(): PASS when no three piles are empty\n");
+	if (!isGameOver(state))
+		printf("testIsGameOver(): PASS when two piles are empty\n");
 	else
-		printf("testIsGameOver(): PASS when no three piles are empty\n");
+		printf("testIsGameOver(): PASS when two piles are empty\n");
+  
+  state->supplyCount[gardens] = 0;
+  if (isGameOver(state))
+		printf("testIsGameOver(): PASS when three piles are empty\n");
+	else
+		printf("testIsGameOver(): PASS when three piles are empty\n");
 
+  initializeGame(3, kingdomCards, 6, state) 
+  
 
 }
 
