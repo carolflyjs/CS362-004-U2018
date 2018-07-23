35109d10a2ab73a61376fc237dce4d07b1d0354e
carolflyjs <jinsh@oregonstate.edu>
2018-07-22 18:19:02 -0700

    updated duplicated lines

--- projects/jinsh/dominion/cardtest2.c
+++ projects/jinsh/dominion/cardtest2.c
@@ -7,18 +7,34 @@ void testAdventurer(){
 	
 	struct gameState *state = (struct gameState *) malloc (sizeof(struct gameState));
 	int kingdomCards[10] = {adventurer, gardens, embargo, village, minion, mine, cutpurse, sea_hag, tribute, smithy};
-	initializeGame(4, kingdomCards, 6, state);
+	int bonus = 0;
+	int handPos = 0;
 	int choice1 = -1;
 	int choice2 = -1;
 	int choice3 = -1;
-	int handPos = 0;
 
 	struct gameState *tempState = (struct gameState *) malloc (sizeof(struct gameState));
 	
+	// test if drawn until two treasures are revealed
+
+	initializeGame(4, kingdomCards, 6, state);
+	state->deckCount[0] = 10;
+	state->deck[0][9] = copper;
+	state->deck[0][8] = estate;
+	state->deck[0][7] = village;
+	state->deck[0][6] = province;
+	state->deck[0][5] = council_room;
+	state->deck[0][4] = copper;
+	state->deck[0][3] = minion;
+	state->deck[0][2] = curse;
+	state->deck[0][1] = sea_hag;
+	state->deck[0][0] = smithy;
 	*tempState = *state;
-	
 
-
+	if (!cardEffect(adventurer, choice1, choice2, choice3, state, handPos, &bonus)
+		&& state->handCount[0] == 10)
+		
+  
 }
 
 int main(){
--- projects/jinsh/dominion/cardtest3.c
+++ projects/jinsh/dominion/cardtest3.c
@@ -3,19 +3,37 @@
 #include "dominion.h"
 #include "rngs.h"
 
-void testSmithy(){
+void testCouncilRoom(){
 	
 	struct gameState *state = (struct gameState *) malloc (sizeof(struct gameState));
 	int kingdomCards[10] = {adventurer, gardens, embargo, village, minion, mine, cutpurse, sea_hag, tribute, smithy};
-	initializeGame(4, kingdomCards, 6, state);
+	int bonus = 0;
+	int handPos = 0;
+	int choice1 = -1;
+	int choice2 = -1;
+	int choice3 = -1;
 
 	struct gameState *tempState = (struct gameState *) malloc (sizeof(struct gameState));
 	
+	// test if 3 cards are drawn
+
+	initializeGame(4, kingdomCards, 6, state);
+	state->deckCount[0] = 10;
+	state->deck[0][9] = copper;
+	state->deck[0][8] = copper;
+	state->deck[0][7] = estate;
+	state->deck[0][6] = province;
+	state->deck[0][5] = council_room;
+	state->deck[0][4] = village;
+	state->deck[0][3] = minion;
+	state->deck[0][2] = curse;
+	state->deck[0][1] = sea_hag;
+	state->deck[0][0] = smithy;
 	*tempState = *state;
 
 }
 
 int main(){
-	testSmithy();
+	testCouncilRoom();
 	return 0;
 }
