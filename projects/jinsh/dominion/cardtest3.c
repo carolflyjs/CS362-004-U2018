ae421c539c1284c284f735f6067a619498c3e88e
Shuang Jin <jinsh@flip3.engr.oregonstate.edu>
2018-07-22 20:09:25 -0700

    completed all tests

--- projects/jinsh/dominion/cardtest3.c
+++ projects/jinsh/dominion/cardtest3.c
@@ -15,24 +15,95 @@ void testCouncilRoom(){
 
 	struct gameState *tempState = (struct gameState *) malloc (sizeof(struct gameState));
 	
-	// test if 3 cards are drawn
+	// test if 4 cards are drawn
 
 	initializeGame(4, kingdomCards, 6, state);
 	state->deckCount[0] = 10;
 	state->deck[0][9] = copper;
-	state->deck[0][8] = copper;
+	state->deck[0][8] = silver;
 	state->deck[0][7] = estate;
 	state->deck[0][6] = province;
-	state->deck[0][5] = council_room;
+	state->deck[0][5] = gardens;
 	state->deck[0][4] = village;
 	state->deck[0][3] = minion;
 	state->deck[0][2] = curse;
 	state->deck[0][1] = sea_hag;
 	state->deck[0][0] = smithy;
+	state->discardCount[0] = 2;
+	state->discard[0][0] = silver;
+	state->discard[0][1] = province;
+	
 	*tempState = *state;
 	
-	//if (!cardEffect(council_room, choice1, choice2, choice3, state, handPos, &bonus)
-		 
+
+	if (!cardEffect(council_room, choice1, choice2, choice3, state, handPos, &bonus)
+		&& state->handCount[0] == 8)
+		printf("testCouncilRoom(): PASS when four cards are drawn\n");
+	else
+		printf("testCouncilRoom(): FAIL when four cards are drawn\n");
+	
+	// test if 4 cards are added to hand
+	if (state->hand[0][5] == copper
+		&& state->hand[0][6] == silver
+		&& state->hand[0][7] == estate
+		&& state->hand[0][8] == province)
+		printf("testCouncilRoom(): PASS when four cards on the top of deck are added to hands\n");
+	else
+		printf("testCouncilRoom(): FAIL when four cards on the top of deck are added to hands\n");
+		
+	// test if num of actions unchanged
+	if (state->numActions == tempState->numActions)
+		printf("testCouncilRoom(): PASS when num of actions unchanged\n");
+	else
+		printf("testCouncilRoom(): FAIL when num of actions unchanged\n");
+
+	// test if num of buys increased
+	if (state->numBuys == tempState->numBuys + 1)
+		printf("testCouncilRoom(): PASS when num of buys increased by one\n");
+	else
+		printf("testCouncilRoom(): FAIL when num of buys increased by one\n");
+	
+	// test if council room is discarded
+	if (state->hand[0][handPos] != council_room)
+		printf("testCouncilRoom(): PASS when council room is discarded\n");
+	else
+		printf("testCouncilRoom(): FAIL when council room is discarded\n");
+	
+	// test if discard piles are not shullfed when not needed
+	if (state->discardCount[0] != 0)
+		printf("testCouncilRoom(): PASS when discard pileis not shuffled when not needed\n");
+	else
+		printf("testCouncilRoom(): FAIL when discard pileis not shuffled when not needed\n");
+
+	// test if discard piles are shuffled when needed
+	initializeGame(4, kingdomCards, 6, state);
+	state->deckCount[0] = 3;
+	state->deck[0][2] = copper;
+	state->deck[0][1] = silver;
+	state->deck[0][0] = estate;
+	state->discardCount[0] = 7;
+	state->discard[0][6] = province;
+	state->discard[0][5] = gardens;
+	state->discard[0][4] = village;
+	state->discard[0][3] = minion;
+	state->discard[0][2] = curse;
+	state->discard[0][1] = sea_hag;
+	state->discard[0][0] = smithy;
+	*tempState = *state;
+	
+
+	if (!cardEffect(council_room, choice1, choice2, choice3, state, handPos, &bonus)
+	&& state->discardCount[0] == 0)
+		printf("testCouncilRoom(): PASS when discard pile is shuffled when needed\n");
+	else
+		printf("testCouncilRoom(): FAIL when discard pile is shuffled when needed\n");
+
+	if (state->handCount[1] - tempState->handCount[1] == 1
+		&& state->handCount[2] - tempState->handCount[2] == 1
+		&& state->handCount[3] - tempState->handCount[3] == 1)
+		printf("testCouncilRoom(): PASS when all other players gain a card\n");
+	else
+		printf("testCouncilRoom(): FAIL when all other players gain a card]n");
 }
 
 int main(){
--- projects/jinsh/dominion/cardtest4.c
+++ projects/jinsh/dominion/cardtest4.c
@@ -15,7 +15,7 @@ void testMine(){
 
 	struct gameState *tempState = (struct gameState *) malloc (sizeof(struct gameState));
 	
-	// test if 3 cards are drawn
+	// test if player can exchange copper with gold
 
 	initializeGame(4, kingdomCards, 6, state);
 	state->deckCount[0] = 10;
@@ -29,9 +29,130 @@ void testMine(){
 	state->deck[0][2] = curse;
 	state->deck[0][1] = sea_hag;
 	state->deck[0][0] = smithy;
+	state->hand[0][0] = mine;
+	state->hand[0][1] = copper;
+	state->hand[0][2] = minion;
+	state->hand[0][3] = silver;
+	state->hand[0][4] = gold;
 	*tempState = *state;
+	
+	if (cardEffect(mine, 1, gold, choice3, state, handPos, &bonus) == -1)
+		printf("testMine(): PASS when player wants to exchange copper with gold\n");
+	else
+		printf("testMine(): FAIL when player wants to exchange copper with gold\n");
 
+	// test if player can exachange silver with gold
+
+	initializeGame(4, kingdomCards, 6, state);
+	state->deckCount[0] = 10;
+	state->deck[0][9] = copper;
+	state->deck[0][8] = copper;
+	state->deck[0][7] = estate;
+	state->deck[0][6] = province;
+	state->deck[0][5] = council_room;
+	state->deck[0][4] = village;
+	state->deck[0][3] = minion;
+	state->deck[0][2] = curse;
+	state->deck[0][1] = sea_hag;
+	state->deck[0][0] = smithy;
+	state->hand[0][0] = mine;
+	state->hand[0][1] = copper;
+	state->hand[0][2] = minion;
+	state->hand[0][3] = silver;
+	state->hand[0][4] = gold;
+	*tempState = *state;
+	
+	if (cardEffect(mine, 1, silver, choice3, state, handPos, &bonus) == 0)
+		printf("testMine(): PASS when player wants to exchange copper with silver\n");
+	else
+		printf("testMine(): FAIL when player wants to exchange copper with silver\n");
+
+	// test if players can exchange copper with silver
+	
+	initializeGame(4, kingdomCards, 6, state);
+	state->deckCount[0] = 10;
+	state->deck[0][9] = copper;
+	state->deck[0][8] = copper;
+	state->deck[0][7] = estate;
+	state->deck[0][6] = province;
+	state->deck[0][5] = council_room;
+	state->deck[0][4] = village;
+	state->deck[0][3] = minion;
+	state->deck[0][2] = curse;
+	state->deck[0][1] = sea_hag;
+	state->deck[0][0] = smithy;
+	state->hand[0][0] = mine;
+	state->hand[0][1] = copper;
+	state->hand[0][2] = minion;
+	state->hand[0][3] = silver;
+	state->hand[0][4] = gold;
+	*tempState = *state;
+	
+	if (cardEffect(mine, 3, gold, choice3, state, handPos, &bonus) == 0)
+		printf("testMine(): PASS when player wants to exchange silver with gold\n");
+	else
+		printf("testMine(): FAIL when player wants to exchange silver with gold\n");
+	
+	// test if numActions unchanged
+	if (state->numActions == tempState->numActions)
+		printf("testMine(): PASS when num of actions unchanged\n");
+	else
+		printf("testMine(): FAIL when num of actions unchanged\n");
+
+	// test if numBuys unchanged
+	if (state->numBuys == tempState->numBuys)
+		printf("testMine(): PASS when num of buys unchanged\n");
+	else
+		printf("testMine(): FAIL when num of buys unchanged\n");
+
+	// test if gained card added to hand
+	if (state->hand[0][5] == gold)
+		printf("testMine(): PASS when gained card added to hand\n");
+	else
+		printf("testMine(): FAIL when gained card added to hand\n");
+
+	// test if discard pile unchanged
+	if (state->discardCount[0] == tempState->discardCount[0])
+		printf("testMine(): PASS when discard pile unchanged\n");
+	else
+		printf("testMine(): FAIL when discard pile unchanged\n");
+	
+	// test if deck pile unchanged
+	if (state->deckCount[0] == tempState->deckCount[0])
+		printf("testMine(): PASS when deck pile unchanged\n");
+	else
+		printf("testMine(): FAIL when deck pile unchanged\n");
+	
+
+	// test if players can exchange a non-treasure card
+
+	initializeGame(4, kingdomCards, 6, state);
+	state->deckCount[0] = 10;
+	state->deck[0][9] = copper;
+	state->deck[0][8] = copper;
+	state->deck[0][7] = estate;
+	state->deck[0][6] = province;
+	state->deck[0][5] = council_room;
+	state->deck[0][4] = village;
+	state->deck[0][3] = minion;
+	state->deck[0][2] = curse;
+	state->deck[0][1] = sea_hag;
+	state->deck[0][0] = smithy;
+	state->hand[0][0] = mine;
+	state->hand[0][1] = copper;
+	state->hand[0][2] = minion;
+	state->hand[0][3] = silver;
+	state->hand[0][4] = gold;
+	*tempState = *state;
+	
+	if (cardEffect(mine, 2, gold, choice3, state, handPos, &bonus) == -1)
+		printf("testMine(): PASS when player wants to exchange non-Treasure card\n");
+	else
+		printf("testMine(): FAIL when player wants to exchange non-Treasure card\n");
+
+	
 }
+
 
 int main(){
 	testMine();
